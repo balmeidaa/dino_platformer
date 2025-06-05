1e87391ca4b8e7b45d8cd892132234861d5063ba
balmeidaa <bj.aa.414@gmail.com>
2025-06-05 12:47:40 -0600

    Improved and fixed bugs on UI

--- Main.gd
+++ Main.gd
@@ -1,17 +1,42 @@
 extends Node2D
 
+onready var menu = $CanvasLayer/UI/Menu
+onready var credits =  $CanvasLayer/UI/Credits 
+onready var label_win =  $CanvasLayer/UI/Menu/Labels/PlayerWin
+onready var label_lose =  $CanvasLayer/UI/Menu/Labels/GameOver
 
 
 func _ready():
-    SignalBus.connect("player_wins", self, "player_win")
+    SignalBus.connect("player_wins", self, "player_wins")
     SignalBus.connect("game_over", self, "game_over")
-    $Debugger.add_property($Dino, "mx_spd", "")
+
  
     
 func player_wins():
-    pass
+    label_win.show()
+    menu.get_node("Labels").show()
+    menu.get_node("Buttons").show()
+   
 
 func game_over():
-    pass
- 
- 
+    label_lose.show()
+    menu.get_node("Labels").show()
+    menu.get_node("Buttons").show()
+
+
+func _on_Play_pressed():
+    get_tree().reload_current_scene()
+    SignalBus.hide_all(menu)
+
+
+func _on_Credits_pressed():
+    SignalBus.hide_all(menu)
+    SignalBus.show_all(credits)
+
+
+
+func _on_back_pressed():
+    SignalBus.hide_all(credits)
+    SignalBus.show_all(menu)
+    label_win.hide()
+    label_lose.hide()
--- util_scripts/SignalBus.gd
+++ util_scripts/SignalBus.gd
@@ -9,3 +9,18 @@ func player_wins():
 
 func game_over():
     emit_signal("game_over")
+
+
+func hide_all(ui_node):
+    var children_nodes = ui_node.get_children()
+    for node in children_nodes:
+        node.hide()
+        
+func show_all(ui_node):
+    var children_nodes = ui_node.get_children()
+    for node in children_nodes:
+        node.show()
+
+func toggle_element(parent_ui, node):
+    pass
+    
